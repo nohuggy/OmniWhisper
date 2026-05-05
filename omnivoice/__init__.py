7c89c3cdc7cce505516315a22d4552bbfe4f6739
Huggy <etallionboy@gmail.coom>
2026-05-06 00:18:55 +0800

    Auto-select omnivoice_kaggle.py when running in Kaggle environment

--- omnivoice/__init__.py
+++ omnivoice/__init__.py
@@ -19,10 +19,20 @@ try:
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
-from omnivoice.models.omnivoice import (
-    OmniVoice,
-    OmniVoiceConfig,
-    OmniVoiceGenerationConfig,
-)
+import os
+
+# Automatically switch to Kaggle-optimized model if in Kaggle environment
+if os.environ.get("KAGGLE_URL_BASE") or os.path.exists("/kaggle"):
+    from omnivoice.models.omnivoice_kaggle import (
+        OmniVoice,
+        OmniVoiceConfig,
+        OmniVoiceGenerationConfig,
+    )
+else:
+    from omnivoice.models.omnivoice import (
+        OmniVoice,
+        OmniVoiceConfig,
+        OmniVoiceGenerationConfig,
+    )
 
 __all__ = ["OmniVoice", "OmniVoiceConfig", "OmniVoiceGenerationConfig"]
